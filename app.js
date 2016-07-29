6cdc6c6ac383a20fd3218f39bd66a31467cf2f48
isoares <soares.itamar@gmail.com>
2016-07-29 16:52:52 -0300

    port and ip

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ var app = express();
 var server = http.createServer(app);
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
+
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
